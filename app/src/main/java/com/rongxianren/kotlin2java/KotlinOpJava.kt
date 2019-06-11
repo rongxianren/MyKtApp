c2def59ba57e9c3a1c39952b6ad551866dd71ceb
wangtangyun <wangtangyun@onething.net>
2019-06-11 20:43:45 +0800

    Kotlin2Java

--- app/src/main/java/com/rongxianren/kotlin2java/KotlinOpJava.kt
+++ app/src/main/java/com/rongxianren/kotlin2java/KotlinOpJava.kt
@@ -13,4 +13,6 @@ fun main(args: Array<String>) {
     val javaObj = JavaArrayExample()
     val array = intArrayOf(0, 1, 2, 3)
     javaObj.removeIndices(array)  // 将 int[] 传给方法
+
+    val clazz = javaObj.javaClass
 }
